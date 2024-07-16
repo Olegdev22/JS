3b94f127e3713a9d7b649182129c1f067fc4cf27
Oleg <example@gmail.com>
2024-07-16 14:41:12 +0300

    деструктуризация

--- js/Object_деструктуризация.js
+++ js/Object_деструктуризация.js
@@ -9,18 +9,27 @@ const options = {
         bg: 'red'
     }
 }
-console.log(options.name)
+// console.log(options.name)
+//
+// delete options.name;
+// console.log(options);
+console.log(`Через метод Object.keys: ${Object.keys(options).length}`)   // узнать сколько пар/ключей в объекте
 
-delete options.name;
-console.log(options);
+const {border, bg} = options.colors// деструткуризация объекта - достать свойства объекта, вложенные в объетк
+console.log(`Результат деструткризации: ${border}`)
 
 // цикл перебора ключ: значение в объекте
-for(let key in options) {
+let counter = 0; // посчитаем сколько всего ключ/значение в объекте со вложеными объектами
+for (let key in options) {
     if (typeof (options[key]) == 'object') {
         for (let value in options[key]) {
             console.log(`Свойство ${value} имеет значение: ${options[key][value]}`);
+            counter++;
         }
     } else {
         console.log(`Свойство ${key} имеет значение: ${options[key]}`);
+        counter++;
     }
+
+    console.log(`Если считать по старинке через счетчик: ${counter} пар`)
 }
